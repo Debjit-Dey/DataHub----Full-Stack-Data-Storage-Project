42877ab62890257959ecc00d6e7f9fd60ae49e5c
Debjit-Dey <debjitdeyofficial6@gmail.com>
2025-11-03 21:56:14 +0530

    email schema length update

--- models/user.models.js
+++ models/user.models.js
@@ -1,30 +1,27 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: true,
-        minLength: [6, "User of minimum length of 6 is required"]
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+    minLength: [6, "User of minimum length of 6 is required"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+    minLength: [4, "Email of minimum length of 12 is required"],
+  },
+  password: {
+    type: String,
 
-    },
-    email:{
-        type: String,
-        unique: true,
-        trim: true,
-        required: true,
-        minLength: [12, "Email of minimum length of 12 is required"]
-    },
-    password:{
-        type: String,
-       
-       
-        required: true,
-        minLength: [6, "Password of minimum length of 6 is required"]
-    }
-
-})
-const userModel = mongoose.model('userModel', userSchema);
+    required: true,
+    minLength: [6, "Password of minimum length of 6 is required"],
+  },
+});
+const userModel = mongoose.model("userModel", userSchema);
 // const a=10;
 // module.exports = a;
 module.exports = userModel;
